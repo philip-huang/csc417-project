59379b8a582c7c308d693e6aa068f1b4aa3f91e1
hanjzh <55895333+hanjzh@users.noreply.github.com>
2021-12-11 18:31:24 -0500

    Primary constraints working with multiple bars, easy to specify.

--- multibar_basic.m
+++ multibar_basic.m
@@ -8,7 +8,10 @@ h = 5; % height of rigid body
 j = 0.25; % joint hole size
 d = 2; % distance of the joint holes away from COM, the joint holes on 
        % both sides are the same distance away from the COM
-m = 5; % mass of the object
+m = 1; % mass of the objects
+
+ks = 100;%spring and damping constants
+kd = 10;
 
 % Define translation frame of each rigid body
 xL = [1;0];
@@ -33,113 +36,103 @@ x = [xL yL];
 
 %% Get the Jacobian
 % system definition!
-root = [0,0,0]';
-relth = [0]; % initial position of the system (6x1)
-q0 = generateInitCoords(root, relth, d); % initial position defined the number of bodies in the system
+
+% % 2-bar case
+% root = [0,0,0]';
+% relth = [0]; % initial position of the system (6x1)
+% q0 = generateInitCoords(root, relth, d); % initial position defined the number of bodies in the system
+% numBod = length(q0)/3;
+
+% 4-bar case
+root = [pi/2,0,0]';
+relth =  [-pi/2 -pi/2 0 0]; % initial position of the system (6x1)
+q0 = generateInitCoords(root, relth, d);
 numBod = length(q0)/3;
 
-% syms p1_a p2_a p1_b p2_b real 
-% each bar has 2 attachment points, there for (numBod)*2 joint points
-% pa = p1_a p2_a
-pa = sym('pa', [1 2], 'real'); % joint coordinates in the body frame***
-% pb = p1_b p2_b
-pb = sym('pb', [1 2], 'real');
+% joint coordinates in the body frame, one constraint between each joint
+% NOTE: Add more joint coordinates as you add more constraints!
+pa = sym('pa', [(numBod-1) 2], 'real');
+pb = sym('pb', [(numBod-1) 2], 'real');
 
-% syms th_a p1_ac p2_ac th_b p1_bc p2_bc real 
-% q_a = [th_a; p1_ac; p2_ac]; 
-% q_b = [th_b; p1_bc; p2_bc];
-% q = [q_a; q_b];
 q = sym('q', [numBod 3], 'real'); % gen coords for rigid body, COM position and orientation about the COM
-
-% syms w_a v1_ac v2_ac w_b v1_bc v2_bc real 
-% qdot_a = [w_a; v1_ac; v2_ac]; 
-% qdot_b = [w_b; v1_bc; v2_bc];
-% qdot = [qdot_a; qdot_b];
 qdot = sym('qdot', [numBod 3], 'real'); % angular velocity and velocity of the rigid body COM in the world frame 
-
-% syms alpha_a a1_ac a2_ac alpha_b a1_bc a2_bc real 
-% qddot_a = [alpha_a; a1_ac; a2_ac]; 
-% qddot_b = [alpha_b; a1_bc; a2_bc];
-% qddot = [qddot_a; qddot_b];
 qddot = sym('qddot', [numBod 3], 'real'); % angular acc and acc of the rigid body COM in the world frame 
 
-% derive Jacobian from position constraint - get the same J
 R2 = @(theta) [cos(theta) -sin(theta);
     sin(theta) cos(theta)];
-% x_a = R2(th_a) * [p1_a; p2_a] + [p1_ac; p2_ac];
-% x_b = R2(th_b) * [p1_b; p2_b] + [p1_bc; p2_bc];
 
 % transform the joint coordinates in the body frame to the world frame
-%xa = R2(q(1))*pa(2, :)' + q(1,2:3)';
-%xb = R2(q(4))*pb(1, :)' + q(2,2:3)';
-xa = R2(q(1,1))*pa' + q(1,2:3)';
-xb = R2(q(2,1))*pb' + q(2,2:3)';
+xa = [];
+xb = [];
+for c = 1:(numBod-1) % for every constraint, body i-1 and body i
+    xa = cat(1,xa,R2(q(c,1))*pa(1,:)' + q(c,2:3)');
+    xb = cat(1,xb,R2(q(c+1,1))*pb(1,:)' + q(c+1,2:3)');
+end
 
 % C(q) = 0
 % Cdot(q) = dC/dq * qdot = 0
 % Cddot(q) = dC/dq * qddot + dCdot/dq * qdot = 0
-
-%Csym = x_a - x_b;
 Csym = xa - xb;
 
 % flatten everything to vectors
-% pa = (reshape(pa,numBod*2*2,1));
-% pb = (reshape(pb,numBod*2*2,1));
-q = (reshape(q',numBod*3,1));
-qdot = (reshape(qdot',numBod*3,1));
-qddot = (reshape(qddot',numBod*3,1));
-Jsym = jacobian(Csym, q);
+pa = reshape(pa',(numBod-1)*2,1);
+pb = reshape(pb',(numBod-1)*2,1);
+q = reshape(q',numBod*3,1);
+qdot = reshape(qdot',numBod*3,1);
+qddot = reshape(qddot',numBod*3,1);
+Jsym = jacobian(Csym', q);
 
 Cdot_sym = Jsym * qdot;
 
-% c = dCdot/dq * qdot = dJ/dq * qdot * qdot
-% only depends on th_a and th_b
-
-m = 1; % uniform mass
-ks = 100;%spring and damping constants
-kd = 10;
 c_sym = jacobian(Cdot_sym, q) * qdot + ks * Csym + kd * Cdot_sym;
-%Cddot_sym = Jsym * qddot + jacobian(Cdot_sym, q) * qdot;
-
-%Cfunc = matlabFunction(Csym);
-%cfunc = matlabFunction(c_sym);
-%Jfunc = matlabFunction(Jsym);
-
 cfuncmod = matlabFunction(c_sym, 'Vars', {pa, pb, q, qdot});
 Jfuncmod = matlabFunction(Jsym, 'Vars', {pa, pb, q});
 
-% Cddotfunc = matlabFunction(Cddot_sym);
-% Cdotfunc = matlabFunction(Cdot_sym);
-
 %% Time Integration
+%Fext = zeros(numBod*3,1); % apply zero external force
 
-Fext = [-10 0 10 10 20 0]';
+% Apply a tip force
+Fext = zeros(numBod*3,1);
+Fext(end-2:end) = [0 15 35]';
+
+% Fext = 10*ones(numBod*3,1); % apply forces on all the joints
+
 dt = 0.1;
 time = 5;
 tall = 1:dt:time;
 stps = time/dt;
 
 I = m * (w * w + h * h) / 12.0;
-M = eye(6)*m;
-M(1, 1) = I;
-M(4, 4) = I;
+M = eye(numBod*3)*m;
+
+for i = 0:numBod-1
+    M(i*3+1,i*3+1) = I;
+end
 
 Asym = Jsym*inv(M)*Jsym';
 Afunc = matlabFunction(Asym, 'Vars', {pa, pb, q});
-% p1_a = 0;
-% p2_a = d;
-% p1_b = 0;
-% p2_b = -d;
-pa = [0 d];
-pb = [0 -d];
 
+% Give the positions of the joint that are being constrained
+pa = zeros((numBod-1)*2*2,1);
+pb = zeros((numBod-1)*2*2,1);
+% NOTE: this is for a connection between each individual joint that forms a
+% serial chain
+for i = 0:(numBod-1)*2-1
+    if(rem(i, 2)==0)
+        pa(i*2+1:i*2+2) = [0; d];
+        pb(i*2+1:i*2+2) = [0; -d];
+    else
+        pa(i*2+1:i*2+2) = [0; -d];
+        pb(i*2+1:i*2+2) = [0; d];
+    end 
+end 
 %% Run 
 q = q0; % initial position of the system
-qdot = [0 0 0 0 0 0]'; % initial velocity of the system
+qdot = zeros(numBod*3,1); % initial velocity of the system
 %           w vx vy ...
-allConstrF = zeros(6,stps);
+allConstrF = zeros(numBod*3,stps);
 % external force only applies for an initial period of time
-extF = zeros(6,stps);
+extF = zeros(numBod*3,stps);
 for t=1:1
     extF(:, t) = Fext;
 end
@@ -154,14 +147,11 @@ ylim([-5 20])
 grid on
 for i=1:size(tall, 2)
     t = tall(i);
-    %J = Jfunc(p1_a,p1_b,p2_a,p2_b,q(1),q(4));
     J = Jfuncmod(pa, pb, q);
 
     [allCOM, allBars, allax] = getAllBars(q,w,h,j,d,m);
     visualizeAllBars(allCOM, allBars, allax);
 
-    %c = cfunc(p1_a, p1_b, p2_a, p2_b, q(2), q(5), q(3), q(6), q(1), q(4), ...
-    %    qdot(2), qdot(5), qdot(3), qdot(6), qdot(1), qdot(4));
     c = cfuncmod(pa, pb, q, qdot);
 
     b = -(J*inv(M)*extF(:, i) + c);
@@ -170,15 +160,7 @@ for i=1:size(tall, 2)
     qddot = inv(M)*J'*lambda + inv(M)*extF(:, i); % (6x1) update the velocity
     allConstrF(:,i) = J'*lambda;
 
-    % debug information
-    %Cddot = Cddotfunc(qddot(2), qddot(5), qddot(3), qddot(6), qddot(1), qddot(4), p1_a, p1_b, p2_a, p2_b, q(1), q(4), qdot(1), qdot(4));
-
     [q, qdot] = forwardeuler(q, qdot, qddot, dt);
-    
-    % debug information
-    %Cdot = Cdotfunc(p1_a, p1_b, p2_a, p2_b, q(1), q(4), qdot(2), qdot(5), qdot(3), qdot(6), qdot(1), qdot(4));
-    %C = Cfunc(p1_a, p1_b, p2_a, p2_b, q(2), q(5), q(3), q(6), q(1), q(4));
-    %q;
 
     frame = getframe(gcf);
     writeVideo(vid,frame);
